bee6ffa58b153a8af43b10c23253a669aa328e54
mayamolodec <maya.molodec@gmail.com>
2019-12-21 14:26:56 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='Phoenix_interpol',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    packages=find_packages(),
+    test_suite='test',
+    install_requires=['numpy>=1.13', 'scipy>=1.0', 'astropy>=3.2.1'],
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+) 
---
+++ tests.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from numpy.testing import assert_array_equal
+import doctest
+import unittest
+
+import Phoenix_interpol
+
+
+class SortTest(unittest.TestCase):
+	def test(self):
+		a = [3,5,0,1,2,4,6,7,8,9]
+		c = [0,1,2,3,4,5,6,7,8,9]
+		b = [1,2,3,4,5,6,7,8,9,0]
+		d = [3,5,0,1,2,4,6,7,8,9]
+
+		a1 = [9,3,5,0,1,2,4,6,7,8]
+		c1 = [9,0,1,2,3,4,5,6,7,8]
+		b1 = [0,1,2,3,4,5,6,7,8,9]
+		d1 = [9,3,5,0,1,2,4,6,7,8]
+    	
+		a2,c2,b2,d2 = sort(a,c,b,d)
+
+		assert_array_equal(a1,a2)
+		assert_array_equal(b1,b2)
+		assert_array_equal(c1,c2)
+		assert_array_equal(d1,d2)
+
+
+
+if __name__=='__main__':
+	unittest.main()
+
+
+
+ 
