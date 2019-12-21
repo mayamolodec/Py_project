4cd1f50bb0573c5fefb0690b4ecac08726a716e1
mayamolodec <maya.molodec@gmail.com>
2019-12-21 14:50:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+with open('README.md') as fh:
+    long_description = fh.read()
+
 setup(
     name='Phoenix_interpol',
     version='1.0',
@@ -8,5 +11,5 @@ setup(
     packages=find_packages(),
     test_suite='test',
     install_requires=['numpy>=1.13', 'scipy>=1.0', 'astropy>=3.2.1'],
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=long_description,
 ) 
